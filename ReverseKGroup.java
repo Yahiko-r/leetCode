1cb18fadba28bd93bdf125d1f5c262a62d52b611
Yahiko-r <254618796@qq.com>
2018-10-04 12:40:03 +0800

    no recursive

--- ReverseKGroup.java
+++ ReverseKGroup.java
@@ -1,73 +1,37 @@
-package leetCode;
 
 public class ReverseKGroup {
-	class ListNode {
-		int val;
-		ListNode next;
+    class ListNode {
+        int val;
+        ListNode next;
 
-		ListNode(int x) {
-			val = x;
-		}
-	}
+        ListNode(int x) {
+            val = x;
+        }
+    }
 
-	public ListNode reverseKGroup(ListNode head, int k) {
-		if (head == null || head.next == null)
-			return head;
-		ListNode pro = null, cur = head, next;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode next, newHead, tail;
+        ListNode pre = new ListNode(0);
+        newHead = pre;
+        pre.next = head;
+        tail = head;
+        int length = 1;
+        while (head.next != null) {
+            length++;
+            head = head.next;
+        }
+        for (int j = 0; j < length / k; j++) {
+            for (int i = 1; i < k; i++) {
+                next = tail.next.next;
+                tail.next.next = pre.next;
+                pre.next = tail.next;
+                tail.next = next;
+            }
+            pre = tail;
+            tail = tail.next;
 
-		int length = 1;
+        }
+        return newHead.next;
 
-		while (head.next != null) {
-			length++;
-			head = head.next;
-		}
-		int loop = length / k;
-		ListNode[] ln = new ListNode[loop];
-		ListNode newHead = null;
-		int j = 0;
-		while (j < loop) {
-			ln[j] = cur;
-			int i = 0;
-			while (i < k) {
-				next = cur.next;
-				cur.next = pro;
-				pro = cur;
-				cur = next;
-				i++;
-			}
-			if (j > 0)
-				ln[j - 1].next = pro;
-			if (j == 0) {
-				newHead = pro;
-			}
-			j++;
-		}
-		if (j == 0)
-			ln[0].next = cur;
-		return newHead;
-	}
-
-	public void test() {
-
-		ListNode p1 = new ListNode(1);
-		ListNode p2 = new ListNode(2);
-		ListNode p3 = new ListNode(3);
-		ListNode p4 = new ListNode(4);
-		ListNode p5 = new ListNode(5);
-		p1.next = p2;
-		p2.next = p3;
-		p3.next = p4;
-		p4.next = p5;
-		ListNode rs = reverseKGroup(p1, 2);
-		while (rs != null) {
-			System.out.println(rs.val);
-			rs = rs.next;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		ReverseKGroup r = new ReverseKGroup();
-		r.test();
-	}
+    }
 }
