a6ab6826ec044d84b01e1aa23d13773bbf54f8d1
Yahiko-r <254618796@qq.com>
2018-11-08 16:06:40 +0800

    注意边界

--- SpiralMatrixII.java
+++ SpiralMatrixII.java
@@ -1,9 +1,37 @@
 public class SpiralMatrixII {
     public int[][] generateMatrix(int n){
-
-        return null;
+        int right=0,left=0,north=0,south=0,num=1;
+        int[][] rs=new int[n][n];
+        while(num<=n*n) {
+            for (int i =  left; i < n - right; i++) {
+                rs[north][i]=num;
+                num++;
+            }
+            if(num>n*n)break;
+            north++;
+            for(int down=north;down<n-south-1;down++){
+                rs[down][n-right-1]=num;
+                num++;
+            }
+            right++;
+            if(num>n*n)break;
+            for(int back=n-right;back>=left;back--){
+                rs[n-south-1][back]=num;
+                num++;
+            }
+            south++;
+            for(int up=n-south-1;up>=north;up--){
+                rs[up][left]=num;
+                num++;
+            }
+            left++;
+        }
+        return rs;
     }
-    public void circle(int start,int n,int[][] m){
 
+    public static void main(String[] args) {
+        SpiralMatrixII s=new SpiralMatrixII();
+        int [][] sd=s.generateMatrix(3);
+        System.out.println(sd);
     }
 }
