678c57fcdf5950d48aea3309c360eacea980eff1
Yahiko-r <254618796@qq.com>
2018-11-19 19:24:17 +0800

    转置之后反转

--- RotateImage.java
+++ RotateImage.java
@@ -1,5 +1,14 @@
 public class RotateImage {
-    public void rotete(int[][] matrix){
-
+    public void rotete(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n-1-i; j++) {
+                int tmp = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = tmp;
+            }
+        }
     }
 }
