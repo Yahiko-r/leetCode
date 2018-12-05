ef93271f3bac4c365b7231070da7abadf562fbe9
Yahiko-r <254618796@qq.com>
2018-12-05 15:56:41 +0800

    去除链表中重复元素

---
+++ RemoveDuplicatesFromList.java
@@ -0,0 +1,21 @@
+public class RemoveDuplicatesFromList {
+    public ListNode deleteDup(ListNode head){
+
+        ListNode newHead=new ListNode(0);
+        newHead.next=head;
+        ListNode pre=newHead;
+        while(head.next!=null){
+            if(head.next.val!=head.val){
+                head=head.next;
+                if(pre.next!=head)
+                    pre=pre.next;
+
+            }else{
+                head=head.next;
+                pre.next=head.next;
+
+            }
+        }
+        return newHead.next;
+    }
+}
