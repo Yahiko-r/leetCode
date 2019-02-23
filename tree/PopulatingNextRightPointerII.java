f78f87f3388f09a77833fdf08661703830ce8192
Yahiko-r <254618796@qq.com>
2019-02-23 16:17:29 +0800

    tree

---
+++ tree/PopulatingNextRightPointerII.java
@@ -0,0 +1,42 @@
+package tree;
+
+
+public class PopulatingNextRightPointerII {
+    public Node connect(Node root){
+        Node head = null; //head of the next level
+        Node prev = null; //下一层链表的前一个
+        Node cur = root;  //current node of current level
+
+        while (cur != null) {
+
+            while (cur != null) { //iterate on the current level
+                //left child
+                if (cur.left != null) {
+                    if (prev != null) {
+                        prev.next = cur.left;
+                    } else {
+                        head = cur.left;
+                    }
+                    prev = cur.left;
+                }
+                //right child
+                if (cur.right != null) {
+                    if (prev != null) {
+                        prev.next = cur.right;
+                    } else {
+                        head = cur.right;
+                    }
+                    prev = cur.right;
+                }
+                //move to next node
+                cur = cur.next;
+            }
+
+            //move to next level
+            cur = head;
+            head = null;
+            prev = null;
+        }
+        return root;
+    }
+}
